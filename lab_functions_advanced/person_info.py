a03a3291e9ef00ff33604b57100ed6b1a9c07f6f
Iskren Dimitrov <114501449+Iskren-Dimitrov@users.noreply.github.com>
2023-02-08 23:21:11 +0200

    Problem Solutions for Lab: lab_functions_advanced
    add_tasks

---
+++ lab_functions_advanced/person_info.py
@@ -0,0 +1,13 @@
+'''Write a function called get_info that receives a name, an age, and a town and returns a string in the format:
+"This is {name} from {town} and he is {age} years old". Use dictionary unpacking when testing your function.\
+Submit only the function in the judge system.
+Example:
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+'''
+
+
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
---
+++ lab_functions_advanced/rectangle.py
@@ -0,0 +1,30 @@
+'''Create a function called rectangle(). It must have two parameters - length and width.
+First, you need to check if the given arguments are integers:
+•	If one/ both of them is/ are NOT an integer/s, return the string "Enter valid values!"
+Create two inner functions:
+•	area() - returns the area of the rectangle with the given length and width
+•	perimeter() - returns the perimeter of the rectangle with the given length and width
+In the end, the rectangle function should return a string containing the area and the perimeter of a rectangle in the\
+following format:
+"Rectangle area: {ract_area}
+Rectangle perimeter: {rect_perim}"
+Example:
+print(rectangle(2, 10))
+'''
+
+
+def rectangle(length, width):
+    def rectangle_area():
+        return length * width
+
+    def rectangle_perimeter():
+        return (length * 2) + (width * 2)
+
+    if type(length) != int or type(width) != int:
+        return "Enter valid values!"
+
+    return f'''Rectangle area: {rectangle_area()}
+Rectangle perimeter: {rectangle_perimeter()}'''
+
+
+print(rectangle(2, 10))
