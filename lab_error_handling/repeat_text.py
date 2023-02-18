ec74220226afa398fecf2edf696c02e8123047ff
Iskren Dimitrov <114501449+Iskren-Dimitrov@users.noreply.github.com>
2023-02-18 21:20:26 +0200

    Problem Solutions for Lab: lab_error_handling
    add_tasks

---
+++ lab_error_handling/repeat_text.py
@@ -0,0 +1,30 @@
+'''Write a program that receives a text on the first line and times (to repeat the text) that must be an integer.\
+If the user passes a non-integer type for the times variable, handle the exception and print a message
+"Variable times must be an integer".
+Example:
+Hello
+Bye
+
+'''
+
+from io import StringIO
+import sys
+
+test_input = '''Hello
+2
+'''
+
+sys.stdin = StringIO(test_input)
+
+
+def repeat_text(text, number):
+    return text * number
+
+
+try:
+    text = input()
+    number = int(input())
+    print(repeat_text(text, number))
+
+except ValueError:
+    print("Variable times must be an integer")
---
+++ lab_error_handling/value_cannot_be_negative.py
@@ -0,0 +1,21 @@
+'''Create your own exception called ValueCannotBeNegative. Write a program that reads five numbers from the console\
+(on separate lines). If a negative number occurs, raise the exception.
+Example:
+1
+4
+-5
+3
+10
+
+'''
+
+
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+numbers = [int(input()) for _ in range(5)]
+
+for num in numbers:
+    if num < 0:
+        raise ValueCannotBeNegative(f'Value cannot be negative number.Convert {num} to {abs(num)}')
